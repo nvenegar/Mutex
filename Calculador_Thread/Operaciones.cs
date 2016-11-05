442fa1b70322d34b82563acea7af9623493e7de8
Nelson Venegas <nvenegar@hotmail.com>
2016-11-04 22:51:52 -0500

    Agregar archivos de proyecto.

---
+++ Calculador_Thread/Operaciones.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace Calculador_Thread
+{
+   class Operaciones
+    {
+        public int operador1;
+        public int operador2;
+        private int resultado;
+        private static Mutex mut = new Mutex();
+
+        // Constructor de la clase, la cual toma valores iniciales
+
+        public Operaciones(int _operador1, int _operador2)
+        {
+            operador1=_operador1;
+            operador2=_operador2;
+        }
+
+        public void Sumar()
+        {
+           mut.WaitOne();
+                Console.WriteLine("{0} Ingresando de Codigo Protegido",
+                Thread.CurrentThread.Name);
+                resultado = operador1 + operador2;
+                //Bloqueamos el Hilo actual durante un segundo 
+                Thread.Sleep(1000);
+                Console.WriteLine("El resultado de la Suma es:" + resultado);
+                Console.WriteLine("{0} Saliendo de Codigo Protegido\r\n",
+                Thread.CurrentThread.Name);
+           mut.ReleaseMutex();
+       }
+
+        public void Restar()
+        {
+          mut.WaitOne();
+                Console.WriteLine("{0} Ingresando de Codigo Protegido",
+                Thread.CurrentThread.Name);
+                resultado = operador1 - operador2;
+                //Bloqueamos el Hilo actual durante un segundo
+                Thread.Sleep(1000);
+                Console.WriteLine("El resultado de la Resta es:" + resultado);
+                Console.WriteLine("{0} Saliendo de Codigo Protegido\r\n",
+                Thread.CurrentThread.Name);
+          mut.ReleaseMutex();
+        }
+
+        public void Multiplicar()
+        {
+            mut.WaitOne();
+                Console.WriteLine("{0} Ingresando de Codigo Protegido",
+                Thread.CurrentThread.Name);
+                resultado = operador1 * operador2;
+                //Bloqueamos el hilo actual durante un segundo
+                Thread.Sleep(1000);
+                Console.WriteLine("El resultado de la Multiplicacion es:" + resultado);
+                Console.WriteLine("{0} Saliendo de Codigo Protegido\r\n",
+                Thread.CurrentThread.Name);
+            mut.ReleaseMutex();
+
+        }
+    }
+}
---
+++ Calculador_Thread/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace Calculador_Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creacion de la instancia de la clase e inicializacion de la misma.
+            Operaciones Op = new Operaciones(5, 7);
+            //Creacion de cada uno de los hilos para cada una de las operaciones de la clase
+            Thread Hilo1 = new Thread(new ThreadStart(Op.Sumar));
+            Hilo1.Name = String.Format("Hilo1");
+            Thread Hilo2 = new Thread(new ThreadStart(Op.Restar));
+            Hilo2.Name = String.Format("Hilo2");
+            Thread Hilo3 = new Thread(new ThreadStart(Op.Multiplicar));
+            Hilo3.Name = String.Format("Hilo3");
+
+            //Inicio de ejecucion de cada uno de los hilos creados
+            Hilo1.Start();
+            Hilo2.Start();
+            Hilo3.Start();
+            //Espera para que el usuario presione una tecla. 
+            Console.ReadKey();
+
+        }
+    }
+}
